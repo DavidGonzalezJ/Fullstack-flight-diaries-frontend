4f5d53a279b6b7f5343ef72f7c85ae3aabe0aed2
David <davidgonzalezj97@gmail.com>
2024-03-28 20:59:42 +0100

    Exercise 9.16 Creating a react app that reanders the diary entries from the backend

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { DiaryEntry } from "./types";
+import Entry from "./Entry";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [entries, setEntries] = useState<DiaryEntry[]>([]);
+  useEffect(() => {
+    axios.get<DiaryEntry[]>('http://localhost:3000/api/diaries').then(response => {
+      setEntries(response.data);
+    })
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h2>Diary entries</h2>
+      {entries.map(entry => <Entry key={entry.id} entry={entry}/>)}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/Entry.tsx
@@ -0,0 +1,13 @@
+import { DiaryEntry } from "./types"
+
+const Entry = ({entry}: {entry:DiaryEntry}) => {
+    return (
+        <>
+            <h3>{entry.date}</h3>
+            <p>visibility: {entry.visibility}<br/>
+            weather: {entry.weather}</p>
+        </>
+    );
+};
+
+export default Entry;
